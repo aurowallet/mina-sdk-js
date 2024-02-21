5e82e3346b5e318269acc46b68c0f3407cca188e
wjdfx <wjdfxa@gmail.com>
2024-02-21 12:42:50 +0800

    Add account test

--- packages/mina-signer/src/account.js
+++ packages/mina-signer/src/account.js
@@ -1,10 +1,9 @@
-import Client from "mina-signer";
-import * as bip39 from "bip39";
 import * as bip32 from "bip32";
-import { Buffer } from "safe-buffer";
+import * as bip39 from "bip39";
 import bs58check from "bs58check";
-import BigNumber from "bignumber.js";
-const decimals = 9;
+import Client from "mina-signer";
+import { Buffer } from "safe-buffer";
+
 function reverse(bytes) {
   const reversed = new Buffer(bytes.length);
   for (let i = bytes.length; i > 0; i--) {
@@ -14,34 +13,18 @@ function reverse(bytes) {
 }
 
 export default {
-  async generateMnemonic() {
+  generateMnemonic() {
     const mnemonic = bip39.generateMnemonic();
     return {
       mnemonic,
     };
   },
-  async isAddressValid({ address }) {
-    try {
-      const decodedAddress = bs58check.decode(address).toString("hex");
-      return !!decodedAddress && decodedAddress.length === 72;
-    } catch (ex) {
-      return false;
-    }
-  },
-  async decodeAddress({ address }) {
-    try {
-      const decodedAddress = bs58check.decode(address).toString("hex");
-      return decodedAddress;
-    } catch (ex) {
-      return null;
-    }
-  },
-  async importWalletByMnemonic({
+  importWalletByMnemonic({
     mnemonic,
     accountIndex = 0,
     needPrivateKey = false,
   }) {
-    const seed = await bip39.mnemonicToSeedSync(mnemonic);
+    const seed = bip39.mnemonicToSeedSync(mnemonic);
     const masterNode = bip32.fromSeed(seed);
     let hdPath = "m/44'/12586'/" + accountIndex + "'/0/0";
     const child0 = masterNode.derivePath(hdPath);
@@ -63,7 +46,7 @@ export default {
     }
     return res;
   },
-  async importWalletByPrivateKey({ privateKey }) {
+  importWalletByPrivateKey({ privateKey }) {
     console.log("privateKey", privateKey);
     const client = new Client({ network: "mainnet" });
     const minaPublicKey = client.derivePublicKey(privateKey);
@@ -73,7 +56,7 @@ export default {
       pubKey: minaPublicKey,
     };
   },
-  async importWallet({ key: mnemonicOrPrivateKey, keyType }) {
+  importWallet({ key: mnemonicOrPrivateKey, keyType }) {
     switch (keyType) {
       case "priKey":
         return this.importWalletByPrivateKey({
@@ -83,84 +66,5 @@ export default {
       default:
         return this.importWalletByMnemonic({ mnemonic: mnemonicOrPrivateKey });
     }
-  },
-
-  /**
-   * 
-   * @param {*} privateKey
-   * @param {*} fromAddress
-   * @param {*} toAddress
-   * @param {*} amount
-   * @param {*} fee
-   * @param {*} nonce
-   * @param {*} memo
-   */
-  async signPayment({
-    privateKey,
-    fromAddress,
-    toAddress,
-    amount,
-    fee,
-    nonce,
-    memo,
-  }) {
-    let signedPayment;
-    try {
-      let keys = {
-        privateKey: privateKey,
-        publicKey: fromAddress,
-      };
-      let decimal = new BigNumber(10).pow(decimals);
-      let sendFee = new BigNumber(fee).multipliedBy(decimal).toNumber();
-      let sendAmount = new BigNumber(amount).multipliedBy(decimal).toNumber();
-      const client = new Client({ network: "mainnet" });
-      signedPayment = client.signPayment(
-        {
-          to: toAddress,
-          from: keys.publicKey,
-          amount: sendAmount,
-          fee: sendFee,
-          nonce: +nonce,
-          memo,
-          validUntil: 4294967295,
-        },
-        keys
-      );
-    } catch (error) {
-      signedPayment = { error: { message: "buildFailed" } };
-    }
-    return signedPayment;
-  },
-  async signStakeDelegation({
-    privateKey,
-    fromAddress,
-    toAddress,
-    fee,
-    nonce,
-    memo,
-  }) {
-    let signedStakingPayment;
-    try {
-      let keys = {
-        privateKey: privateKey,
-        publicKey: fromAddress,
-      };
-      let decimal = new BigNumber(10).pow(decimals);
-      let sendFee = new BigNumber(fee).multipliedBy(decimal).toNumber();
-      const client = new Client({ network: "mainnet" });
-      signedStakingPayment = client.signStakeDelegation(
-        {
-          to: toAddress,
-          from: keys.publicKey,
-          fee: sendFee,
-          nonce: nonce,
-          memo,
-        },
-        keys
-      );
-    } catch (error) {
-      signedStakingPayment = { error: { message: "buildFailed" } };
-    }
-    return signedStakingPayment;
   },
 };
--- packages/mina-signer/test/test.js
+++ packages/mina-signer/test/test.js
@@ -1,6 +1,56 @@
-async function runTests() {
-  await runAccountTest();
+function expect(actual, matcher) {
+  if (actual !== matcher) {
+    throw new Error(`expect ${matcher}, got ${actual}`);
+  }
+}
 
-  console.log("all tests passed.");
+/** account type */
+function runAccountTest() {
+  const mne = account.generateMnemonic();
+  console.log("mne", mne);
+  expect(!!mne, true);
+
+  const testData = window.accountData;
+  console.log("testData", testData[0]);
+  for (let index = 0; index < testData.length; index++) {
+    const mneData = testData[index];
+    console.info(
+      `current is ${index}, total is ${testData.length}, mne: ${mneData.mne}`
+    );
+    for (let j = 0; j < mneData.account.length; j++) {
+      const innerAccount = mneData.account[j];
+
+      let mneParams = {
+        mnemonic: mneData.mne,
+      };
+      mneParams.accountIndex = j;
+      console.log("mneParams", mneParams);
+      const accountWithoutPriKey = account.importWalletByMnemonic(mneParams);
+      console.log("accountWithoutPriKey", accountWithoutPriKey);
+      expect(accountWithoutPriKey.pubKey, innerAccount.pubKey);
+      expect(accountWithoutPriKey.hdIndex, innerAccount.hdIndex);
+
+      mneParams.needPrivateKey = true;
+      const accountWithPriKey = account.importWalletByMnemonic(mneParams);
+      console.log("accountWithPriKey", accountWithPriKey);
+
+      expect(accountWithPriKey.priKey, innerAccount.priKey);
+      expect(accountWithPriKey.pubKey, innerAccount.pubKey);
+
+      const privateKey = {
+        privateKey: accountWithPriKey.priKey,
+      };
+      const walletByPrivateKey = account.importWalletByPrivateKey(privateKey);
+      console.log("walletByPrivateKey", walletByPrivateKey);
+      expect(walletByPrivateKey.priKey, innerAccount.priKey);
+      expect(walletByPrivateKey.pubKey, innerAccount.pubKey);
+    }
+    console.log("loop ==================== ");
+  }
+}
+
+async function runTests() {
+  runAccountTest();
+  console.log("all tests successful.");
 }
 window.runTests = runTests;
